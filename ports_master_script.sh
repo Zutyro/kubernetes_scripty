8e1a34ed3b26566c939561f9a3d90fb33de26d5c
Zutyro <58254459+Zutyro@users.noreply.github.com>
2023-03-01 02:19:20 +0100

    Upravený ports master script
    Jednotlivé příkazy pro pravidla upravené tak, aby fungovali když samotný příkaz iptables není v PATH. Dále upravené uložení pravidla aby fungovalo.

--- ports_master_script.sh
+++ ports_master_script.sh
@@ -2,11 +2,11 @@
 
 apt-get install iptables-persistent
 
-iptables -A INPUT -p tcp --dport 6443 -j ACCEPT
-iptables -A INPUT -p tcp --dport 2379 -j ACCEPT
-iptables -A INPUT -p tcp --dport 2380 -j ACCEPT
-iptables -A INPUT -p tcp --dport 10250 -j ACCEPT
-iptables -A INPUT -p tcp --dport 10259 -j ACCEPT
-iptables -A INPUT -p tcp --dport 10257 -j ACCEPT
+/sbin/iptables -A INPUT -p tcp --dport 6443 -j ACCEPT
+/sbin/iptables -A INPUT -p tcp --dport 2379 -j ACCEPT
+/sbin/iptables -A INPUT -p tcp --dport 2380 -j ACCEPT
+/sbin/iptables -A INPUT -p tcp --dport 10250 -j ACCEPT
+/sbin/iptables -A INPUT -p tcp --dport 10259 -j ACCEPT
+/sbin/iptables -A INPUT -p tcp --dport 10257 -j ACCEPT
 
-iptables-save > /etc/iptables/rules.v4
+/sbin/iptables-save > /etc/iptables/rules.v4
