5738246e985d61f4b1f554bd46337a1394b8b577
Zutyro <58254459+Zutyro@users.noreply.github.com>
2023-03-06 23:10:15 +0100

    Nahrán runtime download script
    Začátek scriptu pro stáhnutí runtime souborů. Script stahuje jak soubory tak jejich sha256 soubory.

---
+++ runtime_download_script.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+
+apt-get install wget
+
+wget \
+https://github.com/containerd/containerd/releases/download/v1.6.15/containerd-1.6.15-linux-amd64.tar.gz \
+&& wget \
+https://github.com/containerd/containerd/releases/download/v1.6.15/containerd-1.6.15-linux-amd64.tar.gz.sha256sum \
+&& wget \
+https://raw.githubusercontent.com/containerd/containerd/main/containerd.service \
+&& wget \
+https://github.com/opencontainers/runc/releases/download/v1.1.4/runc.amd64 \
+&& wget \
+https://github.com/opencontainers/runc/releases/download/v1.1.4/runc.sha256sum \
+&& wget \
+https://github.com/containernetworking/plugins/releases/download/v1.2.0/cni-plugins-linux-amd64-v1.2.0.tgz \
+&& wget \
+https://github.com/containernetworking/plugins/releases/download/v1.2.0/cni-plugins-linux-amd64-v1.2.0.tgz.sha256
