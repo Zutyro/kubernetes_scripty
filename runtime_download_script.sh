9f8afedf98ee95bf30aaddb47f501011a62e2f83
Zutyro <58254459+Zutyro@users.noreply.github.com>
2023-03-13 21:42:13 +0100

    Upravený runtime download script
    Script nyní ověřuje shasum stažených souborů v cyklu. Pokud shasum neodpovídá, stahuje soubory znovu.

--- runtime_download_script.sh
+++ runtime_download_script.sh
@@ -1,28 +1,33 @@
 #! /usr/bin/env bash
 
 apt-get install wget
+mkdir runtime_downloads
 
-wget \
-https://github.com/containerd/containerd/releases/download/v1.6.15/containerd-1.6.15-linux-amd64.tar.gz.sha256sum
+files=("containerd-1.6.15-linux-amd64.tar.gz" "runc.amd64" "cni-plugins-linux-amd64-v1.2.0.tgz")
+file_downloads=("https://github.com/containerd/containerd/releases/download/v1.6.15" "https://github.com/opencontainers/runc/releases/download/v1.1.4" "https://github.com/containernetworking/plugins/releases/download/v1.2.0")
+shasums=("containerd-1.6.15-linux-amd64.tar.gz.sha256sum" "runc.sha256sum" "cni-plugins-linux-amd64-v1.2.0.tgz.sha256")
 
-wget \
-https://github.com/containerd/containerd/releases/download/v1.6.15/containerd-1.6.15-linux-amd64.tar.gz
+for i in $(seq 0 2);
+do
+	echo "stahuji ${files[$i]}"
+	while
+		wget -q -P runtime_downloads \
+		${file_downloads[$i]}/${shasums[$i]}
+		wget -q -P runtime_downloads \
+		${file_downloads[$i]}/${files[$i]}
+		echo "overuji ${files[$i]} sha"
+		cd runtime_downloads
+		shasum -c ./${shasums[$1]}
+		cd ..
+		[ ! $? -eq 0 ]
+	do 
+		echo "${files[$i]} overeni neuspelo, stahuji znovu"
+		rm ./runtime_downloads/$file
+		rm ./runtime_downloads/$shasum
+	done
+	echo "${file[$i]} uspesne stahnut a overen"
+done
 
-
-wget \
+wget -q -P runtime_downloads \
 https://raw.githubusercontent.com/containerd/containerd/main/containerd.service
-
-
-wget \
-https://github.com/opencontainers/runc/releases/download/v1.1.4/runc.sha256sum
-
-wget \
-https://github.com/opencontainers/runc/releases/download/v1.1.4/runc.amd64
-
-wget \
-https://github.com/containernetworking/plugins/releases/download/v1.2.0/cni-plugins-linux-amd64-v1.2.0.tgz.sha2
-
-wget \
-https://github.com/containernetworking/plugins/releases/download/v1.2.0/cni-plugins-linux-amd64-v1.2.0.tgz
-
-
+echo "containerd service stahnut bez overeni"
