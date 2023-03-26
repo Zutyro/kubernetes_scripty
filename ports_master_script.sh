36d94796f018bd5bb6cf3aff57012e0250034b41
Zutyro <58254459+Zutyro@users.noreply.github.com>
2023-03-26 20:06:50 +0200

    Upravený ports master script
    Script upravený pro využití sudo.

--- ports_master_script.sh
+++ ports_master_script.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-apt-get install iptables-persistent
+sudo apt-get -y install iptables-persistent
 
-/sbin/iptables -A INPUT -p tcp --dport 6443 -j ACCEPT
-/sbin/iptables -A INPUT -p tcp --dport 2379 -j ACCEPT
-/sbin/iptables -A INPUT -p tcp --dport 2380 -j ACCEPT
-/sbin/iptables -A INPUT -p tcp --dport 10250 -j ACCEPT
-/sbin/iptables -A INPUT -p tcp --dport 10259 -j ACCEPT
-/sbin/iptables -A INPUT -p tcp --dport 10257 -j ACCEPT
+sudo /sbin/iptables -A INPUT -p tcp --dport 6443 -j ACCEPT
+sudo /sbin/iptables -A INPUT -p tcp --dport 2379 -j ACCEPT
+sudo /sbin/iptables -A INPUT -p tcp --dport 2380 -j ACCEPT
+sudo /sbin/iptables -A INPUT -p tcp --dport 10250 -j ACCEPT
+sudo /sbin/iptables -A INPUT -p tcp --dport 10259 -j ACCEPT
+sudo /sbin/iptables -A INPUT -p tcp --dport 10257 -j ACCEPT
 
-/sbin/iptables-save > /etc/iptables/rules.v4
+sudo /sbin/iptables-save > tee /etc/iptables/rules.v4
