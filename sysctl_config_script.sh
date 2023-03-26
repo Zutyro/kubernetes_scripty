9e6d0cdddf3a3789ef2dc9357c233feece521059
Zutyro <58254459+Zutyro@users.noreply.github.com>
2023-03-26 20:40:26 +0200

    Upravený sysctl config script
    Script upravený pro využivání sudo.

--- sysctl_config_script.sh
+++ sysctl_config_script.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-cat <<EOF | tee /etc/modules-load.d/k8s.conf
+sudo cat <<EOF | tee /etc/modules-load.d/k8s.conf
 overlay
 br_netfilter
 EOF
 
-/sbin/modprobe overlay
-/sbin/modprobe br_netfilter
+sudo /sbin/modprobe overlay
+sudo /sbin/modprobe br_netfilter
 
-cat <<EOF | tee /etc/sysctl.d/k8s.conf
+sudo cat <<EOF | tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-iptables  = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward                 = 1
 EOF
 
-/sbin/sysctl --system
+sudo /sbin/sysctl --system
