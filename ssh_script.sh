533ab3bf3c7943c37c81909e0398183073f94ecc
Zutyro <58254459+Zutyro@users.noreply.github.com>
2023-03-26 20:43:16 +0200

    Upravený ssh script
    Script nyní zvládá se připojit pomocí daného hesla přes ssh, nahrát scripty přes scp a poté je spustit přes sudo. Script obsahuje podmínku pouze pro master node. Jedná se o základ, s jaký se bude psát zbytek scriptu.

--- ssh_script.sh
+++ ssh_script.sh
@@ -1,12 +1,22 @@
 #! /usr/bin/env bash
 
-if [ $# -lt 3 ]
+if [ $# -lt 4 ]
 	then
-		echo "Error: Chybejici argumeny. Je potreba vlozit ip adresu, jmeno a heslo pro ssh."
+		echo "Error: Chybejici argumenty. Je potreba vlozit ip adresu, jmeno a heslo pro ssh, a zda li se jedna o master nebo worker node (1 nebo 2). 4 argumenty celkove."
 		exit
 fi
 
 echo "Vlozena ip adresa: $1"
-echo "Vlozeno heslo: $2"
+echo "Vlozeno jmeno: $2"
+echo "Vlozeno heslo: $3"
 
 sshpass -p $3 ssh $2@$1 "mkdir /home/zutyro/test"
+
+if [ $4 -eq 1 ]
+	then
+		sshpass -p $3 ssh $2@$1 "mkdir /home/zutyro/scripts"
+		sshpass -p $3 scp "/home/zutyro/scripts/ports_master_script.sh" "$2@$1:~/scripts"
+		sshpass -p $3 ssh $2@$1 "echo $3 | sudo -S bash /home/zutyro/scripts/ports_master_script.sh"
+		sshpass -p $3 scp "/home/zutyro/scripts/sysctl_config_script.sh" "$2@$1:~/scripts"
+		sshpass -p $3 ssh $2@$1 "echo $3 | sudo -S bash /home/zutyro/scripts/sysctl_config_script.sh"
+fi
