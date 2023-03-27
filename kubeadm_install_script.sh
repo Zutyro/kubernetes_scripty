aab01c3ba80184395dc4429136fa56b597e5031b
Zutyro <58254459+Zutyro@users.noreply.github.com>
2023-03-27 18:51:10 +0200

    Upravený kubeadm install script
    Script nyní využívá sudo.

--- kubeadm_install_script.sh
+++ kubeadm_install_script.sh
@@ -1,14 +1,14 @@
 #! /usr/bin/env bash
 
-apt-get update
-apt-get install -y curl apt-transport-https ca-certificates
+sudo apt-get update
+sudo apt-get install -y curl apt-transport-https ca-certificates
 
-mkdir /etc/apt/keyrings
+sudo mkdir /etc/apt/keyrings
 
-curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-apt-get update
-apt-get install -y kubelet kubeadm kubectl
-apt-mark hold kubelet kubeadm kubectl
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
