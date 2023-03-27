6016342bdd6a2790cc3a648acbac6e64f1a357d0
Zutyro <58254459+Zutyro@users.noreply.github.com>
2023-03-27 17:45:03 +0200

    Upravený ports worker script
    Script nyní využívá sudo a má opravený piping.

--- ports_worker_script.sh
+++ ports_worker_script.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-apt-get install iptables-persistent
+sudo apt-get -y install iptables-persistent
 
-/sbin/iptables -A INPUT -p tcp --dport 10250 -j ACCEPT
-/sbin/iptables -A INPUT -p tcp -m multiport --dports 30000:32767 -j ACCEPT
+sudo /sbin/iptables -A INPUT -p tcp --dport 10250 -j ACCEPT
+sudo /sbin/iptables -A INPUT -p tcp -m multiport --dports 30000:32767 -j ACCEPT
 
 
-/sbin/iptables-save > /etc/iptables/rules.v4
+sudo /sbin/iptables-save | sudo tee /etc/iptables/rules.v4
