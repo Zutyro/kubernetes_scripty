576b1e3c858321c8c73ee89aa32fdcdf84233e2e
Zutyro <58254459+Zutyro@users.noreply.github.com>
2023-03-27 18:49:48 +0200

    Upravený runtime install script
    Script nyní využívá sudo.

--- runtime_install_script.sh
+++ runtime_install_script.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
-tar Cxzvf /usr/local ./runtime_downloads/containerd-1.6.15-linux-amd64.tar.gz
+sudo tar Cxzvf /usr/local ./runtime_downloads/containerd-1.6.15-linux-amd64.tar.gz
 
-mv ./runtime_downloads/containerd.service /etc/systemd/system/containerd.service
-systemctl daemon-reload
-systemctl enable --now containerd
+sudo mv ./runtime_downloads/containerd.service /etc/systemd/system/containerd.service
+sudo systemctl daemon-reload
+sudo systemctl enable --now containerd
 
-mkdir -p /opt/cni/bin
-tar Cxzvf /opt/cni/bin ./runtime_downloads/cni-plugins-linux-amd64-v1.2.0.tgz
+sudo mkdir -p /opt/cni/bin
+sudo tar Cxzvf /opt/cni/bin ./runtime_downloads/cni-plugins-linux-amd64-v1.2.0.tgz
 
-install -m 755 ./runtime_downloads/runc.amd64 /usr/local/sbin/runc
+sudo install -m 755 ./runtime_downloads/runc.amd64 /usr/local/sbin/runc
 
-mkdir /etc/containerd
-containerd config default > /etc/containerd/config.toml
+sudo mkdir /etc/containerd
+sudo containerd config default > /etc/containerd/config.toml
 
-apt-get install rpl
-rpl "SystemdCgroup = false" "SystemdCgroup = true" /etc/containerd/config.toml
+sudo apt-get -y install rpl
+sudo rpl "SystemdCgroup = false" "SystemdCgroup = true" /etc/containerd/config.toml
 
-systemctl restart containerd
+sudo systemctl restart containerd
