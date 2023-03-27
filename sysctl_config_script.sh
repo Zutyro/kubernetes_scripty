631c8a3be13fcb9142b7bc376802e4377e3a19dc
Zutyro <58254459+Zutyro@users.noreply.github.com>
2023-03-27 17:47:04 +0200

    Upravený sysctl config script
    Script nyní používá tee přes sudo.

--- sysctl_config_script.sh
+++ sysctl_config_script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo cat <<EOF | tee /etc/modules-load.d/k8s.conf
+sudo cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
 overlay
 br_netfilter
 EOF
@@ -8,7 +8,7 @@ EOF
 sudo /sbin/modprobe overlay
 sudo /sbin/modprobe br_netfilter
 
-sudo cat <<EOF | tee /etc/sysctl.d/k8s.conf
+sudo cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-iptables  = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward                 = 1
