e9fd744a43a4f2860c8f9c35e77c2ffbf3001b29
Zutyro <58254459+Zutyro@users.noreply.github.com>
2024-03-28 19:54:57 +0100

    Upravený pro if statement
    Nyní rozlišuje master a worker node podle souboru v argumentu

--- cluster-install.sh
+++ cluster-install.sh
@@ -11,8 +11,27 @@ else
     fi
 fi    
 
+
+master_install () {
+    echo $1
+    echo "Tohle je master node"
+}
+
+worker_install () {
+    echo $1
+    echo "Tohle je worker node"
+}
+
 file="$1"
 
-while IFS= read -r line; do
-    echo "a line: $line"
+while IFS=" " read -r line; do
+    node=($line)
+    if [ ${node[1]} = "master" ]
+    then
+        master_install ${node[0]}
+    fi
+    if [ ${node[1]} = "worker" ]
+    then
+        worker_install ${node[0]}
+    fi
 done < "$file"
