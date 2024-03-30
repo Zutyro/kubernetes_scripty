8d681060438b8c70cc69094d4494feac2ec40226
Zutyro <58254459+Zutyro@users.noreply.github.com>
2024-03-30 23:15:01 +0100

    Upravená funkce pro master node
    V případě master nodu nyní vytvoří nftables pravidla pro otevření potřebných portů

--- cluster-install.sh
+++ cluster-install.sh
@@ -1,25 +1,38 @@
 #! /usr/bin/env bash
 if [ $# -ne 1 ] 
 then
-    echo "Chyba: Argument musi byt jeden"
+    echo Chyba: Argument musi byt jeden
     exit
 else
     if ! [ -f "$1" ] 
     then
-        echo "Chyba: Argument musi byt textovy soubor"
+        echo Chyba: Argument musi byt textovy soubor
         exit
     fi
 fi    
 
 
 master_install () {
-    echo $1
-    echo "Tohle je master node"
+    node=$1
+    echo Vytvareni pravidel pro otevreni portu
+    ssh ${node[0]} "sudo nft add table inet kubernetes"
+    ssh ${node[0]} "sudo nft 'add chain inet kubernetes input { type filter hook input priority 0 ; }'"
+    ssh ${node[0]} "sudo nft add rule inet kubernetes input tcp dport 6443 accept"
+    ssh ${node[0]} "sudo nft add rule inet kubernetes input tcp dport 2379 accept"
+    ssh ${node[0]} "sudo nft add rule inet kubernetes input tcp dport 2380 accept"
+    ssh ${node[0]} "sudo nft add rule inet kubernetes input tcp dport 10250 accept"
+    ssh ${node[0]} "sudo nft add rule inet kubernetes input tcp dport 10259 accept"
+    ssh ${node[0]} "sudo nft add rule inet kubernetes input tcp dport 10257 accept"
+    ssh ${node[0]} 'echo flush ruleset | sudo tee /etc/nftables.conf'
+    ssh ${node[0]} 'sudo nft list ruleset | sudo tee -a /etc/nftables.conf'
+
+
 }
 
 worker_install () {
-    echo $1
-    echo "Tohle je worker node"
+    node=$1
+    echo ${node[0]}
+    echo Tohle je ${node[1]} node
 }
 
 file="$1"
@@ -28,10 +41,10 @@ while IFS=" " read -r line; do
     node=($line)
     if [ ${node[1]} = "master" ]
     then
-        master_install ${node[0]}
+        master_install $node
     fi
     if [ ${node[1]} = "worker" ]
     then
-        worker_install ${node[0]}
+        worker_install $node
     fi
 done < "$file"
