e67bb3f9b61fcdb37e3a92686d224c401a0bfff7
Zutyro <58254459+Zutyro@users.noreply.github.com>
2024-04-15 15:14:32 +0200

    Nahrán script pro přípravu nodů
    Scrip mění hostname nodů předaných skrz soubor. Určený pro klonované virtuální stroje.

---
+++ priprava_nodu.sh
@@ -0,0 +1,58 @@
+#! /usr/bin/env bash
+if [ $# -ne 1 ] 
+then
+    echo Chyba: Argument musi byt jeden
+    exit
+else
+    if ! [ -f "$1" ] 
+    then
+        echo Chyba: Argument musi byt textovy soubor
+        exit
+    fi
+fi    
+
+
+echo Instalace potrebnych nastroju
+
+sudo apt install uuid-runtime
+
+
+master_edit() {
+    old_hostname=$(ssh ${node[0]} -n -o 'StrictHostKeyChecking accept-new' 'echo $HOSTNAME')
+    echo $old_hostname
+    mastercount=$(($mastercount + 1))  
+    echo $mastercount
+    ssh ${node[0]} -n "sudo hostnamectl set-hostname master$mastercount"
+    ssh ${node[0]} -n "sudo sed -i s/$old_hostname/master$mastercount/g /etc/hosts"
+    ssh ${node[0]} -n "sudo shutdown -r now"
+}
+
+worker_edit() {
+    old_hostname=$(ssh ${node[0]} -n -o 'StrictHostKeyChecking accept-new' 'echo $HOSTNAME')
+    echo $old_hostname
+    workercount=$(($workercount + 1))
+    echo $workercount
+    ssh ${node[0]} -n "sudo hostnamectl set-hostname worker$workercount"
+    ssh ${node[0]} -n "sudo sed -i s/$old_hostname/worker$workercount/g /etc/hosts"
+    ssh ${node[0]} -n "sudo shutdown -r now"
+}
+
+
+file="$1"
+
+echo Zmena hostname a uuid vsech nodu
+
+
+while IFS=" " read -r line; do
+    node=($line)
+    if [ ${node[1]} = "master" ]
+    then
+        echo Priprava master nodu
+        master_edit $node
+    fi
+    if [ ${node[1]} = "worker" ]
+    then
+        echo Priprava worker nodu
+        worker_edit $node
+    fi
+done < "$file"
